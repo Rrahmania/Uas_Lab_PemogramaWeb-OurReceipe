1723a035a12ea5e857305a335d804a23eefd8c2a
Rrahmania <rahmaniautami34@gmail.com>
2025-12-04 15:26:31 +0700

    Delete routes directory

--- routes/kategori.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { listKategori } = require("../controllers/kategori.controller");
-
-router.get("/", listKategori);
-
-module.exports = router;
